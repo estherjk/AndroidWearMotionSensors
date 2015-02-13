3845c60ca6e652da76b7397fe593071e83cc3e90
Esther Jun Kim <esther.jun@gmail.com>
2015-02-13 13:42:23 -0800

    Add round and square layouts

--- wear/src/main/java/com/drejkim/androidwearmotionsensors/MainActivity.java
+++ wear/src/main/java/com/drejkim/androidwearmotionsensors/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.DotsPageIndicator;
 import android.support.wearable.view.GridViewPager;
+import android.support.wearable.view.WatchViewStub;
 
 public class MainActivity extends Activity {
 
@@ -13,10 +14,15 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-        final GridViewPager pager = (GridViewPager) findViewById(R.id.pager);
-        pager.setAdapter(new SensorFragmentPagerAdapter(getFragmentManager()));
+        WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override public void onLayoutInflated(WatchViewStub stub) {
+                final GridViewPager pager = (GridViewPager) findViewById(R.id.pager);
+                pager.setAdapter(new SensorFragmentPagerAdapter(getFragmentManager()));
 
-        DotsPageIndicator indicator = (DotsPageIndicator) findViewById(R.id.page_indicator);
-        indicator.setPager(pager);
+                DotsPageIndicator indicator = (DotsPageIndicator) findViewById(R.id.page_indicator);
+                indicator.setPager(pager);
+            }
+        });
     }
 }
